bdd18e3e3f679c0a29f1098f7af56d3db6515a96
syc <syc@partdb.com>
2022-03-23 21:00:19 +0900

    Add: 반환 타입 @tabutabu98

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.dto.MemberDto;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -150,5 +151,29 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        // List는 null로 반환되지 않는다.
+        List<Member> result = memberRepository.findListByUsername("asdfasdf");
+        System.out.println("result = " + result.size());
+
+        // 단건일 경우 결과가 없을 시 null을 반환한다.(Spring Data JPA)
+        Member findMember = memberRepository.findMemberByUsername("asdfasdf");
+        System.out.println("findMember = " + findMember);
+
+        /**
+         * java 8 이후로는 Optional을 사용
+         * 값이 2개 이상이라면 예외가 터진다.
+         * Spring은 여러가지 기술과 연결할 것을 염두하여 동일한 예외를 내려준다.
+         */
+        Optional<Member> findMember2 = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("findMember2 = " + findMember2);
     }
 }
