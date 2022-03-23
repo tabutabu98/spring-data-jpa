7aad2e49fee30dde145ddc16cab414742a8d987a
syc <syc@partdb.com>
2022-03-23 17:09:24 +0900

    Add: 예제 도메인 모델과 동작확인 @tabutabu98

--- src/main/java/study/datajpa/domain/Member.java
+++ src/main/java/study/datajpa/domain/Member.java
@@ -1,30 +1,43 @@
 package study.datajpa.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})   // 연관관계 필드는 안하는것이 좋음
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    /**
-     * Entity는 기본 생성자가 필요하다.
-     * 프록시 기술과 조인하기 위해서
-     */
-    protected Member() {
-    }
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
