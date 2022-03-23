1d01c8ca84cced83831b9533f5f3e0e2f66c7c42
syc <syc@partdb.com>
2022-03-23 18:56:01 +0900

    Add: 공통 인터페이스 분석 @tabutabu98

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,7 +3,9 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.domain.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
+    List<Member> findByUsername(String username);
 }
